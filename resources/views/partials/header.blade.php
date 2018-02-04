a043fd4f4cda376346d7bc3014ac1875daca4ed5
Erycoking_mfalme <erycoking360@gmail.com>
2018-02-04 18:29:02 +0300

    omitted search functionality for later version

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -19,10 +19,10 @@
 	        <a class="nav-link" href="{{ route('crop.create') }}">Add Crop</a>
 	      </li>
 	    </ul>
-	    <form class="form-inline my-2 my-lg-0">
-	      <input class="form-control mr-sm-2" type="text" placeholder="Search">
-	      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-	    </form>
+	    {{--<form class="form-inline my-2 my-lg-0">--}}
+	      {{--<input class="form-control mr-sm-2" type="text" placeholder="Search">--}}
+	      {{--<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>--}}
+	    {{--</form>--}}
 	  </div>
 	</nav>
 </header>
