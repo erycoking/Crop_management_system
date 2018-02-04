2b405502424d4d4be49ef814e63d52cf64d21acb
Erycoking_mfalme <erycoking360@gmail.com>
2018-02-05 00:40:18 +0300

    made a minor change to display a message incase records do not exists

--- resources/views/pages/crop/index.blade.php
+++ resources/views/pages/crop/index.blade.php
@@ -8,43 +8,49 @@
 			<h3 class="lead">All Crops</h3>
 		</div>
 		<div class="crops">
-			<table class="table table-bordered table-hover">
-				<thead class="thead-dark">
-					<tr>
-						<td class="text-center lead">Id</td>
-						<td class="text-center lead">Name</td>
-						<td class="text-center lead">Altitude</td>
-						<td class="text-center lead">Harvesting Method</td>
-						<td class="text-center lead">Harvesting Time</td>
-						<td class="text-center lead">Diseases</td>
-						<td colspan="2" class="text-center lead">Action</td>
-					</tr>
-				</thead>
-				<tbody>
-					@foreach($crop as $key=>$crop)
+			@if(count($crop) > 0)
+				<table class="table table-bordered table-hover">
+					<thead class="thead-dark">
 						<tr>
-							<td>{{ $key }}</td>
-							<td>{{ $crop->name }}</td>
-							<td>{{ $crop->altitude }}</td>
-							<td>{{ $crop->farming_method }}</td>
-							<td>{{ $crop->harvesting_time }}</td>
-							<td>
-								<ul>
-								@foreach($crop->disease as $disease)
-									<li class="text-center">{{ $disease->name }}</li>
-								@endforeach
-								</ul>
-							</td>
-							<td>
-								<a href="{{ route('crop.edit', [$crop]) }}" class="btn btn-primary">Edit</a>
-							</td>
-							<td>
-								<a href="{{ route('delete', [$crop]) }}" class="btn btn-primary">Delete</a>
-							</td>
+							<td class="text-center lead">Id</td>
+							<td class="text-center lead">Name</td>
+							<td class="text-center lead">Altitude</td>
+							<td class="text-center lead">Harvesting Method</td>
+							<td class="text-center lead">Harvesting Time</td>
+							<td class="text-center lead">Diseases</td>
+							<td colspan="2" class="text-center lead">Action</td>
 						</tr>
-					@endforeach
-				</tbody>
-			</table>
+					</thead>
+					<tbody>
+						@foreach($crop as $key=>$crop)
+							<tr>
+								<td>{{ $key }}</td>
+								<td>{{ $crop->name }}</td>
+								<td>{{ $crop->altitude }}</td>
+								<td>{{ $crop->farming_method }}</td>
+								<td>{{ $crop->harvesting_time }}</td>
+								<td>
+									<ul>
+									@foreach($crop->disease as $disease)
+										<li class="text-center">{{ $disease->name }}</li>
+									@endforeach
+									</ul>
+								</td>
+								<td>
+									<a href="{{ route('crop.edit', [$crop]) }}" class="btn btn-primary">Edit</a>
+								</td>
+								<td>
+									<a href="{{ route('delete', [$crop]) }}" class="btn btn-primary">Delete</a>
+								</td>
+							</tr>
+						@endforeach
+					</tbody>
+				</table>
+			@else
+				<div class="row justify-content-center card card-body">
+					<p class="lead text-center">No records found.</p>
+				</div>
+			@endif
 			<div class="row justify-content-center card card-body">
 				<a href="{{ route('crop.create') }}" class="btn btn-primary">Add Crop</a>
 			</div>
